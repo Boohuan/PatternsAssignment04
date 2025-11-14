4e051ae9be74c49a88bed70d8d20ce4b419f60d5
B.Bhuiyan <143551642+Boohuan@users.noreply.github.com>
2025-11-14 00:04:21 -0500

    Added createTable and bindTable
    Added the methods above so when application is run is show all employees right away

--- MVCApplication/MVCApplication/src/main/java/com/example/mvcapplication/views/EmployeeView.java
+++ MVCApplication/MVCApplication/src/main/java/com/example/mvcapplication/views/EmployeeView.java
@@ -6,6 +6,7 @@ import com.example.mvcapplication.controllers.EmployeeProjectController;
 import com.example.mvcapplication.controllers.ProjectController;
 import com.example.mvcapplication.models.Employee;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 
 import java.util.List;
@@ -21,8 +22,32 @@ public class EmployeeView extends AbstractView<Employee, EmployeeController> {
         Button projects = navigation("Project Table", "Projects", () -> new ProjectsView(new ProjectController()));
         //go to employee projects table
         Button activeProj = navigation("Active Projects Table", "Employee Projects", () -> new EmployeeProjectView(new EmployeeProjectController()));
+        createTable();
+        bindTableData();
         HBox navSection = new HBox(12, department, projects, activeProj); //creates a sort of div like structure for the buttons
         this.getChildren().addAll(createSearchBar(), tableView, navSection);
+    }
+
+    private void createTable() {
+        tableView.getColumns().clear();
+
+        TableColumn<Employee, String> firstNameCol = new TableColumn<>("First Name");
+        firstNameCol.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+
+        TableColumn<Employee, String> lastNameCol = new TableColumn<>("Last Name");
+        lastNameCol.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+
+        TableColumn<Employee, Double> salaryCol = new TableColumn<>("Salary");
+        salaryCol.setCellValueFactory(new PropertyValueFactory<>("salary"));
+
+        TableColumn<Employee, Integer> departmentCol = new TableColumn<>("Department");
+        departmentCol.setCellValueFactory(new PropertyValueFactory<>("departmentId"));
+
+        tableView.getColumns().addAll(firstNameCol, lastNameCol, salaryCol, departmentCol);
+    }
+
+    private void bindTableData() {
+        tableView.setItems(controller.getEmployees());
     }
 
     private HBox createSearchBar() {
